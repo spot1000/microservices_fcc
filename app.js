c9642e118bb667030f6b1eb6338173b6172361cf
Adam Lichter <adamlichter@gmail.com>
2017-01-10 22:51:30 -0330

    close to finishing timestamp app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyparser = require('body-parser');
 
 app.get('/', function (req, res) {
   res.send('type in a unix time stamp at the end of this address to output the date of that timestamp, or type in a date at the end of the address to output the unix timestamp');
@@ -7,10 +8,12 @@ app.get('/', function (req, res) {
 
 app.get('/:ID', function (req,res) {
   var id = req.params.ID;
+  if (id == parseInt(id)) {
+    console.log('this is a number');
+  } else {
+    console.log('this is not a number')
+  }
   var timestamp = new Date();
-  timestamp.setTime(id);
-  res.send('unix timestamp: ' + id + 'represented Date: ' + timestamp.getFullYear() + timestamp.getMonth());
-
 
 });
 
